fc5167305ecc7fbf199ba979de62443d17ecc3ee
daviddcho <amazingdcho@gmail.com>
2025-05-25 00:23:09 -0400

    run 8 env in parallel

--- mario.py
+++ mario.py
@@ -2,11 +2,12 @@
 import sys
 from agent import *
 
-agent = DQNAgent()  
-if sys.argv[1] == "train":
-  agent.train() 
-elif sys.argv[1] == "play":
-  agent.load("pretrained_models/model_50000.pth")
-  agent.play()
-else:
-  print("Usage: ./mario.py train | play")
+if __name__ == "__main__":
+  agent = DQNAgent()  
+  if sys.argv[1] == "train":
+    agent.train() 
+  elif sys.argv[1] == "play":
+    agent.load("pretrained_models/model_50000.pth")
+    agent.play()
+  else:
+    print("Usage: ./mario.py train | play")
