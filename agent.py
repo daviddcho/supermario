78139ed294c4cd941453383579dda20db6176f39
daviddcho <amazingdcho@gmail.com>
2025-05-25 20:13:55 -0400

    Revert "run 8 env in parallel"
    This reverts commit fc5167305ecc7fbf199ba979de62443d17ecc3ee.

--- agent.py
+++ agent.py
@@ -1,14 +1,11 @@
 import torch
 from torch import tensor, optim
 import torch.nn as nn
-
 import cv2
 import math
 import numpy as np
 import time
 from tqdm import tqdm
-
-from stable_baselines3.common.vec_env import SubprocVecEnv
 
 from model import DuelingDQN
 from wrappers import *
@@ -16,18 +13,23 @@ from experience_replay import ExperienceReplay, Experience
 from logger import Logger
 import hyperparameters as hp
 
+def ascii_render(rgb):
+  grayscale = rgb.mean(axis=2)  # average over RGB
+  chars = " .:-=+*#%@"
+  step = 256 // len(chars)
+  frame = ""
+  for row in grayscale[::8]:  # downsample vertically
+    line = "".join(chars[min(len(chars) - 1, int(val) // step)] for val in row[::6])
+    frame += line + "\n"
+  print("\033c", end="")  # clear screen
+  print(frame)
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("device: ", device)
 
-def make_env ():
-  def _init():
-    return make_mario("SuperMarioBros-1-1-v3", COMPLEX_MOVEMENT)
-  return _init
-
 class DQNAgent:
   def __init__(self):
-    self.n_envs = 8
-    self.env = SubprocVecEnv([make_env() for _ in range(self.n_envs)])
+    self.env = make_mario("SuperMarioBros-1-1-v3", COMPLEX_MOVEMENT)
   
     self.n_episodes = hp.N_EPISODES
     self.alpha = hp.ALPHA 
@@ -38,7 +40,7 @@ class DQNAgent:
     self.min_exp = hp.MIN_EXP 
     self.target_update_freq = hp.TARGET_UPDATE_FREQ 
 
-    self.input_shape = self.env.observation_space.shape[1:]
+    self.input_shape = self.env.observation_space.shape
     self.n_actions = self.env.action_space.n
 
     self.model = DuelingDQN(self.input_shape, self.n_actions).to(device)
@@ -88,10 +90,10 @@ class DQNAgent:
     Picks action with epsilon-greedy policy
     """
     # Pick best action with DQN 
-    state = tensor(np.float32(state)).to(device)
+    state = tensor(np.float32(state)).unsqueeze(0).to(device)
     q_value = self.model(state)
-    action = q_value.argmax(dim=1)
-    return action.tolist()
+    action = q_value.argmax().item()
+    return action
 
   def train(self):
     """
@@ -102,17 +104,16 @@ class DQNAgent:
       distance = 0
 
       current_state = self.env.reset()
-      done = [False] * self.n_envs
-      while not all(done):
-        step += self.n_envs
+      done = False 
+      while not done:
+        step += 1
 
         action = self.select_action(current_state)
         next_state, reward, done, info = self.env.step(action)
         
-        for i in range(self.n_envs):
-          experience = Experience(current_state[i], action[i], reward[i], next_state[i], done[i])
-          self.replay_memory.push(experience)
-        current_state = next_state
+        experience = Experience(current_state, action, reward, next_state, done)
+        self.replay_memory.push(experience)
+        current_state = next_state 
 
         if (self.replay_memory.length() < self.min_exp):
           continue
@@ -121,13 +122,13 @@ class DQNAgent:
         if (step % self.target_update_freq) == 0:
           self.target_model.load_state_dict(self.model.state_dict())
 
-        if (step % 4*self.n_envs) == 0:
+        if (step % 4) == 0:
           minibatch = self.replay_memory.sample_batch(self.batch_size)
           q_value, loss = self.update_model(minibatch)
         else:
           q_value, loss = None, None
 
-        distance = max([inf['x_pos'] for inf in info] + [distance])
+        distance = max(info['x_pos'], distance)
         self.logger.log_step(reward, distance, q_value, loss)
         
       self.logger.log_episode()
@@ -139,17 +140,6 @@ class DQNAgent:
     """
     When you want to watch Mario play
     """
-    def ascii_render(rgb):
-      grayscale = rgb.mean(axis=2)  # average over RGB
-      chars = " .:-=+*#%@"
-      step = 256 // len(chars)
-      frame = ""
-      for row in grayscale[::8]:  # downsample vertically
-        line = "".join(chars[min(len(chars) - 1, int(val) // step)] for val in row[::6])
-        frame += line + "\n"
-      print("\033c", end="")  # clear screen
-      print(frame)
-
     current_state = self.env.reset()
     done = False
     while not done:    
--- mario.py
+++ mario.py
@@ -2,12 +2,11 @@
 import sys
 from agent import *
 
-if __name__ == "__main__":
-  agent = DQNAgent()  
-  if sys.argv[1] == "train":
-    agent.train() 
-  elif sys.argv[1] == "play":
-    agent.load("pretrained_models/model_50000.pth")
-    agent.play()
-  else:
-    print("Usage: ./mario.py train | play")
+agent = DQNAgent()  
+if sys.argv[1] == "train":
+  agent.train() 
+elif sys.argv[1] == "play":
+  agent.load("pretrained_models/model_50000.pth")
+  agent.play()
+else:
+  print("Usage: ./mario.py train | play")
